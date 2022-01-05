50234a420021302dd2819e2ef83de8228cbb294a
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-05 11:49:08 +0100

    Cleanup Context Provider

--- client/src/context/ContextProvider.jsx
+++ client/src/context/ContextProvider.jsx
@@ -1,28 +1,28 @@
 // Wrapper to provide Context to all the components in the App
-
 import React, {useState} from 'react';
 import Context from './Context';
-import Page01 from '../views/Page01/Page01';
 
 // Destructuring props object to access only children
 export default function ContextProvider({children}) {
 
-    // State variables that will be needed by several components and views
-    const [name, setName] = useState('');
-    const [charOneName, setCharOneName] = useState('')
+    // State variable that will be needed by several components and views to store user inputs
+    const [data, setData] = useState(JSON.parse(localStorage.getItem("storedData")) || null)
+
+    // updating the data
+    const setDataToLocalStorage = (storedData) => {
+            localStorage.setItem("storedData", JSON.stringify(storedData));
+            setData(storedData)
+    }
 
     // Data that will be provided as an object
+    // setDtaToLocalStorage Function and data from state variable
     const providedData = {
-        name,
-        charOneName        
+        setDataToLocalStorage,
+        data     
     }
     
     return (
-        <Context.Provider value={providedData}>
-
-        {children}
-
-        </Context.Provider>
+        <Context.Provider value={providedData}>{children}</Context.Provider>
     )
 }
 
