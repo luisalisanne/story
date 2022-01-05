9cd239f6c831fb3b66483de930cc4bcd08954904
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-05 08:59:17 +0100

    Clean up React initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+import ContextProvider from './context/ContextProvider';
+
+// Import Components
+import Header from "./components/Header/Header";
+// import NextButton from "./components/NextButton/NextButton"
+import Footer from "./components/Footer/Footer";
+
+// Import Views
+import Landing from "./views/Landing/Landing";
+import Page01 from "./views/Page01/Page01"
+import Page02 from "./views/Page02/Page02"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <ContextProvider>
+
+        <Header/>
+
+        <main>
+
+          <Routes>
+
+            <Route path="/" element={<Landing/>}/>
+            <Route path="/page-01" element={<Page01/>}/>
+            <Route path="/page-02" element={<Page02/>}/>
+
+          </Routes>
+
+          {/* <NextButton /> */}
+
+        </main>
+
+        <Footer/>
+
+      </ContextProvider>
+    </Router>
+
   );
 }
 
