99fe486146256f937165d1c586f253ad2effb852
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-05 11:49:39 +0100

    Remove import

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,6 +1,5 @@
 import React, {useState, useContext} from 'react'
 import { Link } from 'react-router-dom'
-import NextButton from '../../components/NextButton/NextButton'
 
 import Context from '../../context/Context'
 
