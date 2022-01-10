bbb13788e1279ec730d64022b1072c25bd1d108b
Luisa-Lisanne Forck <luisalisanne.forck@posteo.de>
2022-01-10 09:49:06 +0100

    Add Conditional rendering to not display the current page on Landing page

--- client/src/components/CurrentPage/CurrentPage.jsx
+++ client/src/components/CurrentPage/CurrentPage.jsx
@@ -6,8 +6,16 @@ export default function CurrentPage() {
     let location = useLocation()
     // console.log(location)
     // console.log((location.pathname).slice(-2))
+    
+    const slicedLocation = location.pathname.slice(-2)
 
-    return (
-            <p>{(location.pathname).slice(-2)}/13</p>
-    )
+    // CONDITIONAL RENDERING to not display the current page on Landing page
+
+    if((slicedLocation.includes('/')===false)) {return (
+            <p>{slicedLocation}/13</p>
+    )} else {
+        return (
+            <p></p>
+        )
+    }
 }
